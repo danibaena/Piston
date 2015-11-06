e399ba5eb2483625e73288f11fc6dcadd0098c31
Jesús García Martínez <jesgarciamartinez@gmail.com>
2015-11-06 12:15:02 +0100

    Changes in airbnb.json - functions is now an array with name fields Changes in Piston.js

--- src/lib/Piston.js
+++ src/lib/Piston.js
@@ -1,27 +1,16 @@
 function Piston() {
 }
-Piston.prototype.specParse = function(specPath){
-	try{
-		specFile = require(specPath);
-	}
-	catch (err){
-		console.log("Not a valid path/Invalid file extension");
-	}
-	return specFile;
-};
-Piston.prototype.extractData = function(specFile, functionName, response){
-	var array = specFile.functions[functionName].extract;
-	var extractedData = [];
-	array.forEach(function(value, index){
-		var a = value.split(".");
-    	while(a.length) { 
-    		extractedData.push(a.shift()); 
-    	}
-		// console.log(response[a.shift()][a.shift()]);
-	});
-	console.log(extractedData);
+
+Piston.prototype.parseSpec = function(specPath){
+	return require(specPath);
 };
 
-// Piston.prototype.parseResponse(response); TO-DO
+Piston.prototype.extractData = function(specFile, functionName, response){
+	var fieldsToExtract = specFile.functions[functionName].extract;
+
+	return fieldsToExtract.map(function(value) {
+		return value.split(".");
+	});
+};
 
 module.exports = Piston;
