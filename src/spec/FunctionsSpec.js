ce3466d03293307e2ac651492cfe831188a364c0
Jesús García Martínez <jesgarciamartinez@gmail.com>
2015-11-10 18:57:26 +0100

    added tests for parseDefaults and createOptionsObject, imported request, modified default in airbnb.json

--- src/spec/FunctionsSpec.js
+++ src/spec/FunctionsSpec.js
@@ -11,12 +11,53 @@ describe("Functions", function(){
     expect(functions.parseFunctions(specObject, functionName))
     .toEqual(jasmine.any(Object));
   });
+
    it("extracts the relevant data from a function", function(){
     var extractedArray = [["user", "first_name"], ["user", "picture_url"]];
     var specObject = specFile.parseSpec("../pistonSpecs/airbnb.json");
     var functionName = "get_user";
     expect(functions.extractData(functions.parseFunctions(specObject, functionName)))
     .toEqual(extractedArray);
+   });
+
+   it("returns a request object with default values from specObject", function(){
+    var specObject = specFile.parseSpec("../pistonSpecs/airbnb.json");
+    expect(functions.parseDefaults(specObject))
+    .toEqual(jasmine.any(Function));
+   });
+
+   it("returns an options object with values from the parsedFunction when there is no option field", function(){
+    var parsedFunction =
+      {
+        "name": "get_user",
+        "path": "v2/users/",
+        "method": "GET",
+        "headers": {
+            "host": "api.airbnb.com",
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0",
+            "If-None-Match": "W/\"0ec4c2f8c1695ca2328859164f9b2485",
+            "Connection": "keep-alive"
+        },
+        "after": false,
+        "extract": [
+            "user.first_name",
+            "user.picture_url"
+        ]
+      }
+
+    expect(functions.createOptionsObject(parsedFunction))
+    .toEqual(
+      {
+        "path": "v2/users/",
+        "method": "GET",
+        "headers": {
+            "host": "api.airbnb.com",
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0",
+            "If-None-Match": "W/\"0ec4c2f8c1695ca2328859164f9b2485",
+            "Connection": "keep-alive"
+        }
+      }
+    );
    });
  });
 });
