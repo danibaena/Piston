23ab7ab24009fcc9d8dc01099dfb9405592eabc7
Daniel Baena <d.baena.hermosa@gmail.com>
2015-11-10 18:32:09 +0100

    Added and tested function "list", so user can see what functions are available within the spec loaded. Also refactored Functions.js, the specObject is now a propiety of Functions object iself.

--- src/lib/Functions.js
+++ src/lib/Functions.js
@@ -1,53 +1,62 @@
 request = require('request');
 
-function Functions() {
+function Functions(specObject){
+    this.specObject = specObject;
 }
-Functions.prototype.parseFunction = function(specObject, functionName){
-	var data = specObject.functions;
+Functions.prototype.list = function() {
+    var data = this.specObject.functions;
+    var names = []
+    for(var key in data) {
+        names[key] = data[key].name;
+    }
+    return names;
+};
+Functions.prototype.parseFunctions = function(functionName){
+    var data = this.specObject.functions;
 
-	for (var key in data) {
-		if (data[key].name === functionName) {
-			return data[key];
-		}
-	}
+    for (var key in data) {
+        if (data[key].name === functionName) {
+            return data[key];
+        }
+    }
 };
 Functions.prototype.extractData = function(parsedFunction){
-	var fieldsToExtract = parsedFunction.extract;
+    var fieldsToExtract = parsedFunction.extract;
 
-	return fieldsToExtract.map(function(value) {
-		return value.split(".");
-	});
+    return fieldsToExtract.map(function(value) {
+        return value.split(".");
+    });
 };
 
 Functions.prototype.buildRequest = function(parsedFunction){
 
-	var options = createOptionsObject(parsedFunction)
-	var request
-	// the place where we will store the request returned by parseDefaults
-	if (Piston.baseRequest)
-		var request = Piston.baseRequest
+    var options = createOptionsObject(parsedFunction)
+    var request
+    // the place where we will store the request returned by parseDefaults
+    if (Piston.baseRequest)
+        var request = Piston.baseRequest
 
-	return request.defaults(options, function(){})
+    return request.defaults(options, function(){})
 };
 
 Functions.prototype.createOptionsObject = function(parsedFunction) {
-	var options = {}
-	// concretar el if
-	if (parsedFunction.options.length)
-		return parsedFunction.options
+    var options = {}
+    // concretar el if
+    if (parsedFunction.options.length)
+        return parsedFunction.options
 
-	var excludedOptions = ['name', 'after', 'extract']
-	for (var key in parsedFunction){
-	 	if (excludedOptions.indexOf(key) === -1) {
-	 		options[key] = parsedFunction[key]
-	 	}
-	}
+    var excludedOptions = ['name', 'after', 'extract']
+    for (var key in parsedFunction){
+        if (excludedOptions.indexOf(key) === -1) {
+            options[key] = parsedFunction[key]
+        }
+    }
 
-	return options
+    return options
 };
 
-Functions.prototype.parseDefaults = function(specObject) {
-	return request.defaults({specObject.default})
+Functions.prototype.parseDefaults = function() {
+    return request.defaults(this.specObject.default)
 };
 
 module.exports = Functions;
--- src/spec/FunctionsSpec.js
+++ src/spec/FunctionsSpec.js
@@ -1,22 +1,29 @@
 describe("Functions", function(){
  var Functions = require("../lib/Functions");
- var functions = new Functions();
+ var functions;
  var SpecFile = require("../lib/Piston");
  var specFile = new SpecFile();
 
- describe("Functions can parse a spec object", function(){
+ beforeEach(function() {
+  var specObject = specFile.parseSpec("../pistonSpecs/airbnb.json");
+  functions = new Functions(specObject);
+});
+
+ describe("can parse a spec object", function(){
    it("Returns the function as an object given a function name", function(){
-    var specObject = specFile.parseSpec("../pistonSpecs/airbnb.json");
     var functionName = "get_user";
-    expect(functions.parseFunctions(specObject, functionName))
+    expect(functions.parseFunctions(functionName))
     .toEqual(jasmine.any(Object));
   });
    it("extracts the relevant data from a function", function(){
     var extractedArray = [["user", "first_name"], ["user", "picture_url"]];
-    var specObject = specFile.parseSpec("../pistonSpecs/airbnb.json");
     var functionName = "get_user";
-    expect(functions.extractData(functions.parseFunctions(specObject, functionName)))
+    expect(functions.extractData(functions.parseFunctions(functionName)))
     .toEqual(extractedArray);
+   });
+   it("creates a list of the functions available in the spec",function(){
+    var expectedFunctions = ["get_user", "login"];
+    expect(functions.list()).toEqual(expectedFunctions);
    });
  });
 });
