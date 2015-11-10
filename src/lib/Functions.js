9a09ac2c8a50cc0e8461309a07526b0a80b3be7e
Jesús García Martínez <jesgarciamartinez@gmail.com>
2015-11-10 15:57:28 +0100

    buildRequest, createOptionsObject, parseDefaults

--- src/lib/Functions.js
+++ src/lib/Functions.js
@@ -1,6 +1,8 @@
+request = require('request');
+
 function Functions() {
 }
-Functions.prototype.parseFunctions = function(specObject, functionName){
+Functions.prototype.parseFunction = function(specObject, functionName){
 	var data = specObject.functions;
 
 	for (var key in data) {
@@ -15,6 +17,37 @@ Functions.prototype.extractData = function(parsedFunction){
 	return fieldsToExtract.map(function(value) {
 		return value.split(".");
 	});
+};
+
+Functions.prototype.buildRequest = function(parsedFunction){
+
+	var options = createOptionsObject(parsedFunction)
+	var request
+	// the place where we will store the request returned by parseDefaults
+	if (Piston.baseRequest)
+		var request = Piston.baseRequest
+
+	return request.defaults(options, function(){})
+};
+
+Functions.prototype.createOptionsObject = function(parsedFunction) {
+	var options = {}
+	// concretar el if
+	if (parsedFunction.options.length)
+		return parsedFunction.options
+
+	var excludedOptions = ['name', 'after', 'extract']
+	for (var key in parsedFunction){
+	 	if (excludedOptions.indexOf(key) === -1) {
+	 		options[key] = parsedFunction[key]
+	 	}
+	}
+
+	return options
+};
+
+Functions.prototype.parseDefaults = function(specObject) {
+	return request.defaults({specObject.default})
 };
 
 module.exports = Functions;
