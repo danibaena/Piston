ccc0ae675a086a87edc31875b861f0cbb805b5ef
Jesús García Martínez <jesgarciamartinez@gmail.com>
2015-11-25 17:56:51 +0100

    added regex in processArguments so it changes more than the first instance; processResponse can now access a single element in an array. Initialized twitter spec.

--- src/lib/spotifyUse.js
+++ src/lib/spotifyUse.js
@@ -2,8 +2,8 @@ var Piston = require("../lib/Piston");
 var specPath = "../pistonSpecs/spotify.json";
 var spotify = new Piston(specPath);
 
-spotify.searchArtist("Muse", "artist");
-// spotify.searchAlbum("ok computer", "album");
+// spotify.searchArtist("Muse", "artist");
+spotify.searchAlbum("ok computer", "album");
 // spotify.getAlbum("0sNOF9WDwhWunNAHPD3Baj");
 // spotify.getAlbumTracks("0sNOF9WDwhWunNAHPD3Baj");
 // spotify.getArtist("0OdUWJ0sBjDrqHygGUXeCF");
