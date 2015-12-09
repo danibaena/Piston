ea0f7448ad686110fa0c4fd5535347c4315bcef8
Daniel Baena <d.baena.hermosa@gmail.com>
2015-12-09 13:01:23 +0100

    Now we save the response inside the Action Object to be used later. Improved examples for different API's. Also added method getResponse in Piston, it needs tests.

--- examples/transportCardMadridUse.js
+++ examples/transportCardMadridUse.js
@@ -2,4 +2,7 @@ var Piston = require("../lib/Piston");
 var specPath = "../pistonSpecs/transportCardMadrid.json";
 var transportCardMadrid = new Piston(specPath);
 
-transportCardMadrid.getCardExpireDate(2510010062804);
+transportCardMadrid.getCardExpireDate(2510010062804)
+  .then(function() {
+    console.log(transportCardMadrid.getResponse());
+  });
