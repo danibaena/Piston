94dff6eb4f9e24e9c0fbda21952e227dddeac660
Daniel Baena <d.baena.hermosa@gmail.com>
2015-12-14 17:38:40 +0100

    Added change suggested by Enrique, now the promise returns data. In consequence, method getResponse is no longer needed. All the examples fixed. Added a new spec for echonest API and slightly modified Spotify spec to extract objects.

--- examples/spotifyUse.js
+++ examples/spotifyUse.js
@@ -3,41 +3,41 @@ var specPath = '../pistonSpecs/spotify.json';
 var spotify = new Piston(specPath);
 
 /*spotify.searchArtist('Bob Marley & The Wailers')
-  .then(function() {
-    console.log(spotify.getResponse());
+  .then(function(data) {
+    console.log(data);
   });*/
 
 /*spotify.searchAlbum('ok computer')
-  .then(function() {
-    console.log(spotify.getResponse());
+  .then(function(data) {
+    console.log(data);
   });*/
 
 /*spotify.getAlbum("0sNOF9WDwhWunNAHPD3Baj")
-  .then(function() {
-    console.log(spotify.getResponse());
+  .then(function(data) {
+    console.log(data);
   });*/
 
-spotify.getAlbumTracks('0sNOF9WDwhWunNAHPD3Baj')
-  .then(function () {
-    console.log(spotify.getResponse());
-  });
+/*spotify.getAlbumTracks('0sNOF9WDwhWunNAHPD3Baj')
+  .then(function (data) {
+    console.log(data);
+  });*/
 
-  /*spotify.getArtist("0OdUWJ0sBjDrqHygGUXeCF")
-    .then(function() {
-      console.log(spotify.getResponse());
-    });*/
+/*spotify.getArtist("0OdUWJ0sBjDrqHygGUXeCF")
+  .then(function(data) {
+    console.log(data);
+  });*/
 
-  /*spotify.getUser("tuggareutangranser")
-    .then(function() {
-      console.log(spotify.getResponse());
-    });*/
+/*spotify.getUser("tuggareutangranser")
+  .then(function(data) {
+    console.log(data);
+  });*/
 
-  /*spotify.searchTrack('Redemption Song')
-    .then(function() {
-      console.log(spotify.getResponse());
-    });*/
+/*spotify.searchTrack('Redemption Song')
+  .then(function(data) {
+    console.log(data);
+  });*/
 
-  // spotify.search('Exodus', 'album', 'albums')
-  //   .then(function () {
-  //     console.log(spotify.getResponse());
-  //   });
+// spotify.search('Exodus', 'album', 'albums')
+//   .then(function (data) {
+//     console.log(data);
+//   });
--- examples/transportCardMadridUse.js
+++ examples/transportCardMadridUse.js
@@ -3,6 +3,6 @@ var specPath = "../pistonSpecs/transportCardMadrid.json";
 var transportCardMadrid = new Piston(specPath);
 
 transportCardMadrid.getCardExpireDate(2510010062804)
-  .then(function() {
-    console.log(transportCardMadrid.getResponse());
+  .then(function(data) {
+    console.log(data);
   });
