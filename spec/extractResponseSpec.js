0afa5c3776decd26dc063bda6f32a77202986dc0
Jesús García Martínez <jesgarciamartinez@gmail.com>
2015-12-14 14:12:09 +0100

    extractResponse works + tests

--- spec/extractResponseSpec.js
+++ spec/extractResponseSpec.js
@@ -3,73 +3,164 @@
 describe('can process a response', () => {
   let test = require('../lib/post-process/extractResponse.js');
 
-  it('and splits the fields to extract', function () {
-    let result = [
-      ['user', 'first_name'],
-      ['user', 'picture_url']
-    ];
-
-    let fields = ['user.first_name', 'user.picture_url'];
-    expect(test.splitField(fields)).toEqual(result);
-  });
-
-  it('extracts a single field from a response (object) with an extract field (array)', function () {
+  it('extracts a field from a response (object) with an extract field (array)', function () {
     let response = {a: 'hello'};
     let specExtract = ['a'];
-    let result = ['hi'];
-    expect(test.extract(specExtract, response)).toEqual(result);
+    // let result = ['hi'];
+    let result = ['hello'];
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
 
   });
-  it('extracts a single field from a response (object) with an extract field (object)', function () {
+  it('extracts a field from a response (object) with an extract field (object)', function () {
     let response = {a: 'hello'};
     let specExtract = {a: 'a'};
-    let result = {a: 'hi'};
-    expect(test.extract(specExtract, response)).toEqual(result);
+    // let result = {a: 'hi'};
+    let result = {a: 'hello'};
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
 
   });
   it('extracts two fields from a response (object) with an extract field (array)', function () {
     let response = {a: 'hello', b: 'world'};
     let specExtract = ['a', 'b'];
-    let result = ['hi', 'hi'];
-    expect(test.extract(specExtract, response)).toEqual(result);
+    // let result = ['hi', 'hi'];
+    let result = ['hello', 'world'];
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
 
   });
+
   it('extracts two fields from a response (object) with an extract field (object)', function () {
     let response = {a: 'hello', b: 'world'};
-    let specExtract = {a: 'a', b: ' b'};
-    let result = {a: 'hi', b: 'hi'};
-    expect(test.extract(specExtract, response)).toEqual(result);
+    let specExtract = {a: 'a', b: 'b'};
+    // let result = {a: 'hi', b: 'hi'};
+    let result = {a: 'hello', b: 'world'};
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
 
   });
-// describe('can process a response,', function () {
-//   var response = {
-//     'user': {
-//       'first_name': 'José Antonio',
-//       'picture_url': 'https://a2.muscache.com/'
-//     }
-//   };
-//
-//   const extractedData = [
-//     ['user', 'first_name'],
-//     ['user', 'picture_url']
-//   ];
-//
-//   const path = 'user.first_name';
-//
-//   it('if extractedData is undefined returns the response', function () {
-//     var extractedData = undefined;
-//
-//     expect(currentActions.processResponse(response, extractedData))
-//       .toEqual(response);
-//   });
-//
-//   it('if response is an object and there are only nested objects. ' +
-//   'It returns an array.',
-//     function () {
-//       expect(currentActions.getProperties(response, path))
-//         .toEqual([
-//           'José Antonio'
-//         ]);
-//     });
-// });
+
+  it('extracts two fields from a response (array) with an extract field (object)', function () {
+    let response = [{a: 'hello', b: 'world'}, {a: 'foo', b: 'bar'}];
+    let specExtract = {a: 'a', b: 'b'};
+    // let result = [{a: 'hi', b: 'hi'}, {a: 'hi', b: 'hi'}];
+    let result = [{a: 'hello', b: 'world'}, {a: 'foo', b: 'bar'}];
+    expect(test.extractFromArray(response, specExtract)).toEqual(result);
+
+  });
+
+  it('extracts two fields from a response (array) with an extract field (array)', function () {
+    let response = [{a: 'hello', b: 'world'}, {a: 'foo', b: 'bar'}];
+    let specExtract = ['a', 'b'];
+    let result = [['hello', 'world'], ['foo', 'bar']];
+    expect(test.extractFromArray(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts a nested field from a response (object) with an extract field (object)', function () {
+    let response = {a: {b: 'hello'}};
+    let specExtract = {a: 'a.b'};
+    let result = {a: 'hello'};
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts a nested field from a response (object) with an extract field (array)', function () {
+    let response = {a: {b: 'hello'}};
+    let specExtract = ['a.b'];
+    let result = ['hello'];
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts a nested field from a response (array) with an extract field (object)', function () {
+    let response = [{a: {b: 'hello'}}, {a: {b: 'world'}}];
+    let specExtract = {a: 'a.b'};
+    let result = [{a: 'hello'}, {a: 'world'}];
+    expect(test.extractFromArray(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts a nested field from a response (array) with an extract field (array)', function () {
+    let response = [{a: {b: 'hello'}}, {a: {b: 'world'}}];
+    let specExtract = ['a.b'];
+    let result = [['hello'], ['world']];
+    expect(test.extractFromArray(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts a nested field from a response (array) with an extract field (array)', function () {
+    let response = [{a: {b: 'hello'}}, {a: {b: 'world'}}];
+    let specExtract = ['a.b'];
+    let result = [['hello'], ['world']];
+    expect(test.extractFromArray(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts a field (array) from a response (object) with an extract field (array)', function () {
+    let response = {a: {b: ['hello', 'world']}};
+    let specExtract = ['a.b'];
+    let result = [['hello', 'world']];
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts a field (array) from a response (object) with an extract field (object)', function () {
+    let response = {a: {b: ['hello', 'world']}};
+    let specExtract = {a: 'a.b'};
+    let result = {a: ['hello', 'world']};
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts a field (object) nested inside a field (array) from a response (object) with an extract field (object)', function () {
+    let response = {a: {b: [{c: 'hello'}, {c: 'world'}]}};
+    let specExtract = {a: 'a.b.c'};
+    let result = {a: ['hello', 'world']};
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts the first field (object) nested inside a field (array) from a response (object) with an extract field (object)', function () {
+    let response = {a: {b: [{c: 'hello'}, {c: 'world'}]}};
+    let specExtract = {a: 'a.b.0.c'};
+    let result = {a: 'hello'};
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts a field (object) nested inside a field (array) from a response (object) with an extract field (array)', function () {
+    let response = {a: {b: [{c: 'hello'}, {c: 'world'}]},
+    d: 'foo'};
+    let specExtract = ['a.b.c', 'd'];
+    let result = [['hello', 'world'], 'foo'];
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts the first field (object) nested inside a field (array) from a response (object) with an extract field (object)', function () {
+    let response = {a: {b: [{c: 'hello'}, {c: 'world'}]},
+    d: 'foo'};
+    let specExtract = ['a.b.0.c', 'd'];
+    let result = ['hello', 'foo'];
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts a field (array) nested inside a field (array) from a response (object) with an extract field (object)', function () {
+    let response = {a: {b: [{c: ['hello', 'world']}, {c: ['foo', 'bar']}]},
+    d: 'foo'};
+    let specExtract = {a: 'a.b.c', b: 'd'};
+    let result = {a: [['hello', 'world'], ['foo', 'bar']], b: 'foo'};
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts a field (array) nested inside a field (array) from a response (object) with an extract field (array)', function () {
+    let response = {a: {b: [{c: ['hello', 'world']}, {c: ['foo', 'bar']}]},
+    d: 'foo'};
+    let specExtract = ['a.b.c', 'd'];
+    let result = [[['hello', 'world'], ['foo', 'bar']], 'foo'];
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts a field (array) nested inside a field (object) nested inside a field (array) from a response (object) with an extract field (object)', function () {
+    let response = {a: {b: [{c: {d: ['hello', 'world']}}, {c: {d: ['foo', 'bar']}}]},
+    d: 'foo'};
+    let specExtract = {a: 'a.b.c.d', b: 'd'};
+    let result = {a: [['hello', 'world'], ['foo', 'bar']], b: 'foo'};
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
+  });
+
+  it('extracts a field (array) nested inside a field (object) nested inside a field (array) from a response (object) with an extract field (array)', function () {
+    let response = {a: {b: [{c: {d: ['hello', 'world']}}, {c: {d: ['foo', 'bar']}}]},
+    d: 'foo'};
+    let specExtract = ['a.b.c.d', 'd'];
+    let result = [[['hello', 'world'], ['foo', 'bar']], 'foo'];
+    expect(test.extractFromObject(response, specExtract)).toEqual(result);
+  });
 });
