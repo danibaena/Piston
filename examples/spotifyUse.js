1c8958a0df653fb2ea1d598637487884211b26e1
Jesús García Martínez <jesgarciamartinez@gmail.com>
2015-12-16 08:22:54 +0100

    added features: optionally passing extract as a parameter, optionally passing a callback. Removed bluebird, request is now promisified with node promises

--- examples/spotifyUse.js
+++ examples/spotifyUse.js
@@ -3,42 +3,46 @@ let Piston = require('../lib/Piston');
 let specPath = '../pistonSpecs/spotify.json';
 let spotify = new Piston(specPath);
 
-spotify.searchArtist('Bob Marley & The Wailers')
+// spotify.searchArtist('Bob Marley & The Wailers', function (err, response, body) {
+//   console.log(body);
+// });
+
+// )
+// .then(function (data) {
+//   console.log(data);
+// });
+
+spotify.searchAlbum('ok computer', 'albums.items.name')
   .then(function (data) {
     console.log(data);
   });
 
-/*spotify.searchAlbum('ok computer')
-  .then(function(data) {
-    console.log(data);
-  });*/
+  /*spotify.getAlbum("0sNOF9WDwhWunNAHPD3Baj")
+    .then(function(data) {
+      console.log(data);
+    });*/
 
-/*spotify.getAlbum("0sNOF9WDwhWunNAHPD3Baj")
-  .then(function(data) {
-    console.log(data);
-  });*/
+  /*spotify.getAlbumTracks('0sNOF9WDwhWunNAHPD3Baj')
+    .then(function (data) {
+      console.log(data);
+    });*/
 
-/*spotify.getAlbumTracks('0sNOF9WDwhWunNAHPD3Baj')
-  .then(function (data) {
-    console.log(data);
-  });*/
+  /*spotify.getArtist("0OdUWJ0sBjDrqHygGUXeCF")
+    .then(function(data) {
+      console.log(data);
+    });*/
 
-/*spotify.getArtist("0OdUWJ0sBjDrqHygGUXeCF")
-  .then(function(data) {
-    console.log(data);
-  });*/
+  /*spotify.getUser("tuggareutangranser")
+    .then(function(data) {
+      console.log(data);
+    });*/
 
-/*spotify.getUser("tuggareutangranser")
-  .then(function(data) {
-    console.log(data);
-  });*/
+  /*spotify.searchTrack('Redemption Song')
+    .then(function(data) {
+      console.log(data);
+    });*/
 
-/*spotify.searchTrack('Redemption Song')
-  .then(function(data) {
-    console.log(data);
-  });*/
-
-/*spotify.search('Exodus', 'album', 'albums')
-  .then(function (data) {
-    console.log(data);
-  });*/
+  /*spotify.search('Exodus', 'album', 'albums')
+    .then(function (data) {
+      console.log(data);
+    });*/
