ddc31a7c5a39552726ade442d86addbed9e6d140
Daniel Baena <d.baena.hermosa@gmail.com>
2015-12-16 21:05:18 +0100

    Finished feature to use yaml format.

--- examples/pistonYamlUse.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-let Piston = require("../lib/Piston");
-let specPath = "../pistonSpecs/spotify.yaml";
-let spotify = new Piston(specPath);
-
-spotify.searchArtist('Rosendo', function (err, response, body) {
-  console.log(body);
-});
