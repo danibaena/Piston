73e262f50979e2bec09c0dbafda2335c93922181
Daniel Baena <d.baena.hermosa@gmail.com>
2015-12-17 10:35:24 +0100

    Added tests for new feature of parsing YAML files with Piston

--- spec/PistonSpec.js
+++ spec/PistonSpec.js
@@ -1,34 +1,37 @@
-'use strict'
+'use strict';
 describe('Piston', function () {
   let Piston = require('../lib/Piston');
   let currentPiston;
   let specPath;
 
-  beforeEach(function () {
-    specPath = '../pistonSpecs/airbnb.json';
-    currentPiston = new Piston(specPath);
-  });
+  describe('can create a valid specObject', function () {
+    
+    it('from a json file', function(){
+      specPath = '../pistonSpecs/airbnb.json';
+      expect(new Piston(specPath)).toEqual(jasmine.any(Object));
+    });
 
-  describe('can use a json file', function () {
-    it('Throws an error if file path is invalid', function () {
+    it('from a yaml file', function(){
+      specPath = './pistonSpecs/spotify.yml';
+      expect(new Piston(specPath)).toEqual(jasmine.any(Object));
+    });
+
+    it('or it throws an error if file path is invalid', function () {
       specPath = '';
       expect(function () {new Piston(specPath);}).toThrow();
     });
 
-    it('Throws an error if file extension is not json', function () {
+    it('or throws an error if file extension is not valid', function () {
       specPath = __dirname + '/dummys/airbnb.piston';
       expect(function () {new Piston(specPath);}).toThrow();
-    });
-
-    it('Returns a valid object', function () {
-      specPath = '../pistonSpecs/airbnb.json';
-      expect(new Piston(specPath)).toEqual(jasmine.any(Object));
     });
   });
 
   describe('can create action from keys', function () {
     it('correctly creates properties from a action object', function () {
       let functionName = 'getUser';
+      specPath = '../pistonSpecs/airbnb.json';
+      currentPiston = new Piston(specPath);
       expect(currentPiston[functionName]).toEqual(jasmine.any(Function));
     });
   });
