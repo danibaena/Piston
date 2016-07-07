4f62b90018b67402f28d189af8075df12bafe5fa
dani_baena <daniel.baena@ezzing.com>
2016-07-07 15:35:33 +0200

    Bump version, and bugfix.

--- examples/airbnbUse.js
+++ examples/airbnbUse.js
@@ -3,15 +3,15 @@ let Pistonify = require('../lib/Pistonify');
 let specPath = '../pistonSpecs/airbnb.json';
 let airbnb = new Pistonify(specPath);
 
-// airbnb.getUser(1)
-//   .then(function (data) {
-//     console.log('getUser data: ', data);
-//   });
+ airbnb.getUser(1)
+   .then(function (data) {
+     console.log('getUser data: ', data);
+   });
 
-airbnb.login('i1264348@trbvm.com', '123456')
-  .then(function (data) {
-    console.log('login data: ', data);
-    // console.log(airbnb.stateObject);
-  });
+//airbnb.login('i1264348@trbvm.com', '123456')
+//  .then(function (data) {
+//    console.log('login data: ', data);
+//    // console.log(airbnb.stateObject);
+//  });
 
 
